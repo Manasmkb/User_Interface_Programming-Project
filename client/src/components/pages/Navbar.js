80490cda88773f1babc7c1bf6c05c0a4eea13fd8
ManasBanerjee <banerjem1@newpaltz.edu>
2026-04-24 23:49:47 -0400

    Logged in user - only the Profile link should be available on your navbar. With no current user logged in - only the Login and Register links should be available on your navbar.

--- client/src/components/pages/Navbar.js
+++ client/src/components/pages/Navbar.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import UserContext from "../../context/userContext";
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand navbar-dark custom-navbar px-3">
-    <Link className="navbar-brand" to="/">MyApp</Link>
+const Navbar = () => {
+  const { user } = useContext(UserContext);
 
-    <div className="navbar-nav ms-auto">
-      <Link className="nav-link" to="/login">Login</Link>
-      <Link className="nav-link" to="/register">Register</Link>
-      <Link className="nav-link" to="/profile">Profile</Link>
-    </div>
-  </nav>
-);
+  return (
+    <nav className="navbar navbar-expand navbar-dark custom-navbar px-3">
+      <Link className="navbar-brand" to="/">MyApp</Link>
+
+      <div className="navbar-nav ms-auto">
+        {user?.authenticated ? (
+          <Link className="nav-link" to="/profile">Profile</Link>
+        ) : (
+          <>
+            <Link className="nav-link" to="/login">Login</Link>
+            <Link className="nav-link" to="/register">Register</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
